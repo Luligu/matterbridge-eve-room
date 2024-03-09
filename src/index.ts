1ca1b295a8cd063e69730996a49b2a6f8ef5d735
Luligu <132135057+Luligu@users.noreply.github.com>
2024-03-09 22:01:06 +0100

    1.0.1

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Matterbridge } from 'matterbridge';
 import { AnsiLogger } from 'node-ansi-logger';
-import { EveEatherPlatform } from './platform.js';
+import { EveRoomPlatform } from './platform.js';
 
 /**
  * This is the standard interface for MatterBridge plugins.
@@ -9,5 +9,5 @@ import { EveEatherPlatform } from './platform.js';
  * @param matterbridge - An instance of MatterBridge
  */
 export default function initializePlugin(matterbridge: Matterbridge, log: AnsiLogger) {
-  return new EveEatherPlatform(matterbridge, log);
+  return new EveRoomPlatform(matterbridge, log);
 }
--- src/platform.ts
+++ src/platform.ts
@@ -1,9 +1,20 @@
-import { DeviceTypes, EveHistory, PressureMeasurement, RelativeHumidityMeasurement, TemperatureDisplayUnits, TemperatureMeasurement, WeatherTrend, logEndpoint } from 'matterbridge';
+import {
+  AirQuality,
+  DeviceTypeDefinition,
+  DeviceTypes,
+  EveHistory,
+  RelativeHumidityMeasurement,
+  TemperatureDisplayUnits,
+  TemperatureMeasurement,
+  airQualitySensor,
+  TvocMeasurement,
+  logEndpoint,
+} from 'matterbridge';
 
 import { Matterbridge, MatterbridgeDevice, MatterbridgeAccessoryPlatform, MatterHistory } from 'matterbridge';
 import { AnsiLogger } from 'node-ansi-logger';
 
-export class EveEatherPlatform extends MatterbridgeAccessoryPlatform {
+export class EveRoomPlatform extends MatterbridgeAccessoryPlatform {
   constructor(matterbridge: Matterbridge, log: AnsiLogger) {
     super(matterbridge, log);
   }
@@ -11,38 +22,36 @@ export class EveEatherPlatform extends MatterbridgeAccessoryPlatform {
   override async onStart(reason?: string) {
     this.log.info('onStart called with reason:', reason ?? 'none');
 
-    const history = new MatterHistory(this.log, 'Eve weather', { filePath: this.matterbridge.matterbridgeDirectory });
+    const history = new MatterHistory(this.log, 'Eve room', { filePath: this.matterbridge.matterbridgeDirectory });
 
-    const weather = new MatterbridgeDevice(DeviceTypes.TEMPERATURE_SENSOR);
-    weather.createDefaultIdentifyClusterServer();
-    weather.createDefaultBasicInformationClusterServer('Eve weather', '0x84286475', 4874, 'Eve Systems', 1, 'Eve Weather 20EBS9901', 6650, '3.2.1', 1, '1.1');
-    weather.createDefaultTemperatureMeasurementClusterServer(20 * 100);
+    const room = new MatterbridgeDevice(DeviceTypeDefinition(airQualitySensor));
+    room.createDefaultIdentifyClusterServer();
+    room.createDefaultBasicInformationClusterServer('Eve room', '0x84224975', 4874, 'Eve Systems', 0x27, 'Eve Room 20EAM9901', 1416, '3.2.1', 1, '1.1');
+    room.createDefaultAirQualityClusterServer(AirQuality.AirQualityType.Good);
+    room.createDefaultTvocMeasurementClusterServer();
 
-    weather.addDeviceType(DeviceTypes.HUMIDITY_SENSOR);
-    weather.createDefaultRelativeHumidityMeasurementClusterServer(50 * 100);
+    room.addDeviceType(DeviceTypes.TEMPERATURE_SENSOR);
+    room.createDefaultTemperatureMeasurementClusterServer(20 * 100);
 
-    weather.addDeviceType(DeviceTypes.PRESSURE_SENSOR);
-    weather.createDefaultPressureMeasurementClusterServer(950);
+    room.addDeviceType(DeviceTypes.HUMIDITY_SENSOR);
+    room.createDefaultRelativeHumidityMeasurementClusterServer(50 * 100);
 
-    weather.createDefaultPowerSourceRechargableBatteryClusterServer(87);
-    weather.createDefaultPowerSourceConfigurationClusterServer(1);
+    //room.createDefaultPowerSourceRechargableBatteryClusterServer(87);
+    //room.createDefaultPowerSourceConfigurationClusterServer(1);
 
     // Add the EveHistory cluster to the device as last cluster!
-    weather.createWeatherEveHistoryClusterServer(history, this.log);
-    history.autoPilot(weather);
+    room.createRoomEveHistoryClusterServer(history, this.log);
+    history.autoPilot(room);
 
-    await this.registerDevice(weather);
+    await this.registerDevice(room);
 
-    weather.addCommandHandler('identify', async ({ request: { identifyTime } }) => {
+    room.addCommandHandler('identify', async ({ request: { identifyTime } }) => {
       this.log.warn(`Command identify called identifyTime:${identifyTime}`);
-      logEndpoint(weather);
+      logEndpoint(room);
       history.logHistory(false);
     });
 
-    weather.getClusterServerById(EveHistory.Cluster.id)?.setElevationAttribute(250); // Elevation in mt
-    weather.getClusterServerById(EveHistory.Cluster.id)?.setWeatherTrendAttribute(WeatherTrend.SUN);
-    weather.getClusterServerById(EveHistory.Cluster.id)?.setTemperatureDisplayUnitsAttribute(TemperatureDisplayUnits.CELSIUS);
-    weather.getClusterServerById(EveHistory.Cluster.id)?.setLastPressureAttribute(950);
+    room.getClusterServerById(EveHistory.Cluster.id)?.setTemperatureDisplayUnitsAttribute(TemperatureDisplayUnits.CELSIUS);
 
     let minTemperature = 0;
     let maxTemperature = 0;
@@ -50,25 +59,24 @@ export class EveEatherPlatform extends MatterbridgeAccessoryPlatform {
 
     setInterval(
       () => {
+        const airquality = AirQuality.AirQualityType.Good;
+        const voc = history.getFakeLevel(0, 1000, 0);
         const temperature = history.getFakeLevel(10, 30, 2);
         if (minTemperature === 0) minTemperature = temperature;
         if (maxTemperature === 0) maxTemperature = temperature;
         minTemperature = Math.min(minTemperature, temperature);
         maxTemperature = Math.max(maxTemperature, temperature);
         const humidity = history.getFakeLevel(1, 99, 2);
-        const pressure = history.getFakeLevel(700, 1100, 1);
-        weather.getClusterServerById(TemperatureMeasurement.Cluster.id)?.setMeasuredValueAttribute(temperature * 100);
-        weather.getClusterServerById(RelativeHumidityMeasurement.Cluster.id)?.setMeasuredValueAttribute(humidity * 100);
-        weather.getClusterServerById(PressureMeasurement.Cluster.id)?.setMeasuredValueAttribute(pressure);
-
-        // The Eve app doesn't read the pressure from the PressureMeasurement cluster (Home app doesn't have it!), so we set it in the EveHistory cluster
-        weather.getClusterServerById(EveHistory.Cluster.id)?.setLastPressureAttribute(pressure);
+        room.getClusterServerById(AirQuality.Cluster.id)?.setAirQualityAttribute(airquality);
+        room.getClusterServerById(TvocMeasurement.Cluster.id)?.setMeasuredValueAttribute(voc);
+        room.getClusterServerById(TemperatureMeasurement.Cluster.id)?.setMeasuredValueAttribute(temperature * 100);
+        room.getClusterServerById(RelativeHumidityMeasurement.Cluster.id)?.setMeasuredValueAttribute(humidity * 100);
 
         history.setMaxMinTemperature(maxTemperature, minTemperature);
-        history.addEntry({ time: history.now(), temperature, humidity, pressure });
-        this.log.info(`Set temperature: ${temperature} (min: ${minTemperature} max: ${maxTemperature}) humidity: ${humidity} pressure: ${pressure}`);
+        history.addEntry({ time: history.now(), airquality, voc, temperature, humidity });
+        this.log.info(`Set airquality: ${airquality} voc: ${voc} temperature: ${temperature} (min: ${minTemperature} max: ${maxTemperature}) humidity: ${humidity}`);
       },
-      60 * 1000 - 100,
+      60 * 1000 - 700,
     );
   }
 
