8ac9259181cbb06cd481aac7506a7b5d10f9a287
Luligu <132135057+Luligu@users.noreply.github.com>
2024-03-17 19:49:11 +0100

    Update platform.ts

--- src/platform.ts
+++ src/platform.ts
@@ -1,20 +1,13 @@
-import {
-  AirQuality,
-  DeviceTypeDefinition,
-  DeviceTypes,
-  EveHistory,
-  RelativeHumidityMeasurement,
-  TemperatureDisplayUnits,
-  TemperatureMeasurement,
-  airQualitySensor,
-  TvocMeasurement,
-  logEndpoint,
-} from 'matterbridge';
+import { AirQuality, DeviceTypes, EveHistory, RelativeHumidityMeasurement, TemperatureDisplayUnits, TemperatureMeasurement, airQualitySensor, TvocMeasurement } from 'matterbridge';
 
 import { Matterbridge, MatterbridgeDevice, MatterbridgeAccessoryPlatform, MatterHistory } from 'matterbridge';
 import { AnsiLogger } from 'node-ansi-logger';
 
 export class EveRoomPlatform extends MatterbridgeAccessoryPlatform {
+  room: MatterbridgeDevice | undefined;
+  history: MatterHistory | undefined;
+  interval: NodeJS.Timeout | undefined;
+
   constructor(matterbridge: Matterbridge, log: AnsiLogger) {
     super(matterbridge, log);
   }
@@ -22,71 +15,73 @@ export class EveRoomPlatform extends MatterbridgeAccessoryPlatform {
   override async onStart(reason?: string) {
     this.log.info('onStart called with reason:', reason ?? 'none');
 
-    const history = new MatterHistory(this.log, 'Eve room', { filePath: this.matterbridge.matterbridgeDirectory });
+    this.history = new MatterHistory(this.log, 'Eve room', { filePath: this.matterbridge.matterbridgeDirectory });
 
-    const room = new MatterbridgeDevice(airQualitySensor);
-    room.createDefaultIdentifyClusterServer();
-    room.createDefaultBasicInformationClusterServer('Eve room', '0x84224975', 4874, 'Eve Systems', 0x27, 'Eve Room 20EAM9901', 1416, '1.2.11', 1, '1.0.0');
-    room.createDefaultAirQualityClusterServer(AirQuality.AirQualityType.Good);
-    room.createDefaultTvocMeasurementClusterServer();
+    this.room = new MatterbridgeDevice(airQualitySensor);
+    this.room.createDefaultIdentifyClusterServer();
+    this.room.createDefaultBasicInformationClusterServer('Eve room', '0x84224975', 4874, 'Eve Systems', 0x27, 'Eve Room 20EAM9901', 1416, '1.2.11', 1, '1.0.0');
+    this.room.createDefaultAirQualityClusterServer(AirQuality.AirQualityType.Good);
+    this.room.createDefaultTvocMeasurementClusterServer();
 
-    room.addDeviceType(DeviceTypes.TEMPERATURE_SENSOR);
-    room.createDefaultTemperatureMeasurementClusterServer(20 * 100);
+    this.room.addDeviceType(DeviceTypes.TEMPERATURE_SENSOR);
+    this.room.createDefaultTemperatureMeasurementClusterServer(20 * 100);
 
-    room.addDeviceType(DeviceTypes.HUMIDITY_SENSOR);
-    room.createDefaultRelativeHumidityMeasurementClusterServer(50 * 100);
+    this.room.addDeviceType(DeviceTypes.HUMIDITY_SENSOR);
+    this.room.createDefaultRelativeHumidityMeasurementClusterServer(50 * 100);
 
     //room.createDefaultPowerSourceRechargableBatteryClusterServer(87);
     //room.createDefaultPowerSourceConfigurationClusterServer(1);
 
     // Add the EveHistory cluster to the device as last cluster!
-    room.createRoomEveHistoryClusterServer(history, this.log);
-    history.autoPilot(room);
+    this.room.createRoomEveHistoryClusterServer(this.history, this.log);
+    this.history.autoPilot(this.room);
 
-    await this.registerDevice(room);
+    await this.registerDevice(this.room);
 
-    room.addCommandHandler('identify', async ({ request: { identifyTime } }) => {
+    this.room.addCommandHandler('identify', async ({ request: { identifyTime } }) => {
       this.log.warn(`Command identify called identifyTime:${identifyTime}`);
-      logEndpoint(room);
-      history.logHistory(false);
+      this.history?.logHistory(false);
     });
 
-    room.getClusterServerById(EveHistory.Cluster.id)?.setTemperatureDisplayUnitsAttribute(TemperatureDisplayUnits.CELSIUS);
+    this.room.getClusterServerById(EveHistory.Cluster.id)?.setTemperatureDisplayUnitsAttribute(TemperatureDisplayUnits.CELSIUS);
+
+    this.history.setMaxMinTemperature(20, 20);
+  }
+
+  override async onConfigure() {
+    this.log.info('onConfigure called');
 
     let minTemperature = 0;
     let maxTemperature = 0;
-    history.setMaxMinTemperature(maxTemperature, minTemperature);
 
-    setInterval(
+    this.interval = setInterval(
       () => {
+        if (!this.room || !this.history) return;
         const airquality = AirQuality.AirQualityType.Good;
-        const voc = history.getFakeLevel(0, 1000, 0);
-        const temperature = history.getFakeLevel(10, 30, 2);
+        const voc = this.history.getFakeLevel(0, 1000, 0);
+        const temperature = this.history.getFakeLevel(10, 30, 2);
         if (minTemperature === 0) minTemperature = temperature;
         if (maxTemperature === 0) maxTemperature = temperature;
         minTemperature = Math.min(minTemperature, temperature);
         maxTemperature = Math.max(maxTemperature, temperature);
-        const humidity = history.getFakeLevel(1, 99, 2);
-        room.getClusterServerById(AirQuality.Cluster.id)?.setAirQualityAttribute(airquality);
-        room.getClusterServerById(TvocMeasurement.Cluster.id)?.setMeasuredValueAttribute(voc);
-        room.getClusterServerById(TemperatureMeasurement.Cluster.id)?.setMeasuredValueAttribute(temperature * 100);
-        room.getClusterServerById(TemperatureMeasurement.Cluster.id)?.setMinMeasuredValueAttribute(minTemperature * 100);
-        room.getClusterServerById(TemperatureMeasurement.Cluster.id)?.setMaxMeasuredValueAttribute(maxTemperature * 100);
-        room.getClusterServerById(RelativeHumidityMeasurement.Cluster.id)?.setMeasuredValueAttribute(humidity * 100);
+        const humidity = this.history.getFakeLevel(1, 99, 2);
+        this.room.getClusterServerById(AirQuality.Cluster.id)?.setAirQualityAttribute(airquality);
+        this.room.getClusterServerById(TvocMeasurement.Cluster.id)?.setMeasuredValueAttribute(voc);
+        this.room.getClusterServerById(TemperatureMeasurement.Cluster.id)?.setMeasuredValueAttribute(temperature * 100);
+        this.room.getClusterServerById(TemperatureMeasurement.Cluster.id)?.setMinMeasuredValueAttribute(minTemperature * 100);
+        this.room.getClusterServerById(TemperatureMeasurement.Cluster.id)?.setMaxMeasuredValueAttribute(maxTemperature * 100);
+        this.room.getClusterServerById(RelativeHumidityMeasurement.Cluster.id)?.setMeasuredValueAttribute(humidity * 100);
 
-        history.setMaxMinTemperature(maxTemperature, minTemperature);
-        history.addEntry({ time: history.now(), airquality, voc, temperature, humidity });
+        this.history.setMaxMinTemperature(maxTemperature, minTemperature);
+        this.history.addEntry({ time: this.history.now(), airquality, voc, temperature, humidity });
         this.log.info(`Set airquality: ${airquality} voc: ${voc} temperature: ${temperature} (min: ${minTemperature} max: ${maxTemperature}) humidity: ${humidity}`);
       },
       60 * 1000 - 700,
     );
   }
 
-  override async onConfigure() {
-    this.log.info('onConfigure called');
-  }
-
   override async onShutdown(reason?: string) {
     this.log.info('onShutdown called with reason:', reason ?? 'none');
+    clearInterval(this.interval);
   }
 }
